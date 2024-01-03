cdeab2babe01265211429859e9c3b03d672b3d86
gruntz <77045413+gruntz@users.noreply.github.com>
2024-01-03 10:31:07 +0200

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! Krasi changed something here......";
+		return "Greetings from Spring Boot! Krasi changed goes here.";
 	}
 
 }
